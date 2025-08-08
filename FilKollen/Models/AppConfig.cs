e4d3ab7a832f4f1a4de6b40af737fa2763eab0fb
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2025-08-08 15:29:35 +0200

    Create AppConfig.cs

---
+++ FilKollen/Models/AppConfig.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace FilKollen.Models
+{
+    public class AppConfig
+    {
+        public bool AutoDelete { get; set; } = false;
+        public int QuarantineDays { get; set; } = 30;
+        public bool EnableScheduling { get; set; } = false;
+        public ScheduleFrequency Frequency { get; set; } = ScheduleFrequency.Daily;
+        public TimeSpan ScheduledTime { get; set; } = new TimeSpan(2, 0, 0);
+        public List<string> ScanPaths { get; set; } = new();
+        public List<string> SuspiciousExtensions { get; set; } = new();
+        public List<string> WhitelistPaths { get; set; } = new();
+        public bool ShowNotifications { get; set; } = true;
+        public bool PlaySoundAlerts { get; set; } = false;
+    }
+    
+    public enum ScheduleFrequency
+    {
+        Daily,
+        Weekly,
+        Monthly
+    }
+}
